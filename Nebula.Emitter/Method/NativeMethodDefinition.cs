53c673c4a3bcb08a77cbd84ca47aa460d7513418
Planet <Fabioruscica@hotmail.it>
2025-02-06 21:25:12 +0100

    [Nebula.Emitter] Include native functions in debug information

---
+++ Nebula.Emitter/Method/NativeMethodDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Nebula.CodeEmitter
+{
+    public sealed class NativeMethodDefinition
+        : ISupportsComments
+    {
+        public HashSet<string> LeadingComments { get; } = [];
+        public string Name { get; }
+
+        public NativeMethodDefinition(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- Nebula.Emitter/Types/TypeReference.cs
+++ Nebula.Emitter/Types/TypeReference.cs
@@ -4,8 +4,8 @@
     {
         public static TypeReference Unknown { get; } = new(TypeIdentifier.Unknown);
         public static TypeReference Void { get; } = new(TypeIdentifier.Void);
-        public static TypeReference Char { get; } = new(TypeIdentifier.Char);
-        public static TypeReference Bool { get; } = new(TypeIdentifier.Bool);
+        //public static TypeReference Char { get; } = new(TypeIdentifier.Char);
+        public static TypeReference Bool { get; } = new(TypeIdentifier.Int32);
         public static TypeReference Int { get; } = new(TypeIdentifier.Int32);
         public static TypeReference Float { get; } = new(TypeIdentifier.Float);
         public static TypeReference String { get; } = new(TypeIdentifier.String);
