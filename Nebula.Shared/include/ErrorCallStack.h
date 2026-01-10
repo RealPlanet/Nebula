6626c12422a2c86e957404e4ccee4784885cbfb1
Planet <fabioruscica@hotmail.it>
2026-01-10 16:33:12 +0100

    [Nebula.Shared] ErrorCallStack now attempts to fetch information from debug and original source script when generating information text

--- Nebula.Shared/include/ErrorCallStack.h
+++ Nebula.Shared/include/ErrorCallStack.h
@@ -10,34 +10,42 @@
 
 namespace nebula::shared
 {
-    class ErrorCallStackLine
-    {
-    public:
-        ErrorCallStackLine(size_t instNumber, const std::string& scriptText);
+	class ErrorCallStackLine
+	{
+	public:
+		ErrorCallStackLine(const std::string& scriptSource, const std::string& funcName, size_t instNumber, const std::string& scriptText);
 
-        inline size_t InstructionNumber() const { return m_InstructionNumber; }
-        inline const std::string& Text() const { return m_ScriptLine; }
+		inline size_t GetInstructionNumber() const { return m_InstructionNumber; }
+		inline const std::string& GetScriptSource() const { return m_ScriptSource; }
+		inline const std::string& GetFunctionName() const { return m_FunctionName; }
+		inline const std::string& GetText() const { return m_ScriptLine; }
 
-    private:
-        size_t m_InstructionNumber;
-        std::string m_ScriptLine;
-    };
+	private:
+		const std::string m_ScriptSource;
+		const std::string m_FunctionName;
+		size_t m_InstructionNumber;
+		// Readable script line
+		std::string m_ScriptLine;
+	};
 
-    class ErrorCallStack
-    {
-    public:
-        ErrorCallStack() = default;
+	class ErrorCallStack
+	{
+	public:
+		ErrorCallStack() = default;
 
-        inline void Append(const ErrorCallStackLine& line) { m_Lines.push_back(line); }
-        inline void SetExplanation(InstructionErrorCode errorCode, const std::string& s) { m_ErrorCode = errorCode;  m_preText = s; }
-        inline void Clear() { m_Lines.clear(); }
-        std::string GetAsText() const;
-        InstructionErrorCode GetErrorCode() const { return m_ErrorCode; }
-    private:
-        std::vector<ErrorCallStackLine> m_Lines;
-        std::string m_preText;
-        InstructionErrorCode m_ErrorCode{ InstructionErrorCode::Unknown };
-    };
+		inline void Append(const ErrorCallStackLine& line) { m_Lines.push_back(line); }
+		inline void SetExplanation(InstructionErrorCode errorCode, const std::string& s) { m_ErrorCode = errorCode;  m_preText = s; }
+		inline void Clear() { m_Lines.clear(); }
+		std::string GetAsText() const;
+		InstructionErrorCode GetErrorCode() const { return m_ErrorCode; }
+
+	private:
+		std::vector<ErrorCallStackLine> m_Lines;
+		std::string m_preText;
+		InstructionErrorCode m_ErrorCode{ InstructionErrorCode::Unknown };
+
+		bool GetErrorCallStackLineFromDbgFile(const ErrorCallStackLine& line, std::string* outTextLine) const;
+	};
 }
 
 #endif // !_H_NEBULA_ERROR_CALLSTACK_
--- Nebula.Shared/src/ErrorCallStack.cpp
+++ Nebula.Shared/src/ErrorCallStack.cpp
@@ -1,51 +1,129 @@
 #include <sstream>
 #include <format>
+#include <fstream>
 #include <string>
 
 #include "ErrorCallStack.h"
 #include "Utility.h"
+#include "DebugServer.h"
 
 using namespace nebula::shared;
 
-ErrorCallStackLine::ErrorCallStackLine(size_t instNumber, const std::string& scriptText)
-    : m_InstructionNumber{ instNumber }, m_ScriptLine{ scriptText }
+static inline std::string get_line_at(std::ifstream& stream, size_t line, std::string& prev, std::string& next) {
+	while (line-- > 1) {
+		std::getline(stream, prev);
+	}
+
+	std::string l;
+	std::getline(stream, l);
+	if (!stream.eof()) {
+		std::getline(stream, next);
+	}
+	else {
+		next = "";
+	}
+	return l;
+}
+
+ErrorCallStackLine::ErrorCallStackLine(const std::string& scriptSource, const std::string& funcName, size_t instNumber, const std::string& scriptText)
+	: m_ScriptSource{ scriptSource }, m_FunctionName{ funcName }, m_InstructionNumber{ instNumber }, m_ScriptLine{ scriptText }
 {
 }
 
 std::string ErrorCallStack::GetAsText() const
 {
-    std::stringstream ss;
+	std::stringstream ss;
 
-    ss << "~~~~~~ Interpreter fatal error report: ~~~~~~\n";
+	ss << "~~~~~~ Interpreter fatal error report: ~~~~~~\n";
 
-    if (m_preText != "")
-    {
-        ss << "Error description: ";
-        ss << m_preText << "\n";
-    }
+	if (m_preText != "")
+	{
+		ss << "Error description: ";
+		ss << m_preText << "\n";
+	}
 
-    const char* rootPrefix = "Root: ";
-    size_t rootPrefixCount = cstrlen(rootPrefix);
+	const char* rootPrefix = "Root: ";
+	size_t rootPrefixCount = cstrlen(rootPrefix);
 
-    ss << "See callstack for error location: \n";
-    for (size_t i{0}; i < m_Lines.size(); i++)
-    {
-        auto& line = m_Lines[i];
+	ss << "See callstack for error location: \n";
 
-        std::string labelCountPrefix = std::format("+ @{}", line.InstructionNumber());
+	for (size_t i{ 0 }; i < m_Lines.size(); i++)
+	{
+		auto& line = m_Lines[i];
 
-        if (i == m_Lines.size() - 1)
-        {
-            // Root of call stack
-            ss << rootPrefix << labelCountPrefix << "::" << line.Text() << "" << "\n";
-            continue;
-        }
+		std::string strLine;
+		if (GetErrorCallStackLineFromDbgFile(line, &strLine))
+		{
+			ss << strLine;
+			continue;
+		}
 
-        std::string strLine = std::format("{}::{}\n", labelCountPrefix, line.Text());
-        strLine.insert(0, rootPrefixCount, ' ');
-        ss << strLine;
-    }
-    ss << "~~~~~~ End of Error ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+		std::string labelCountPrefix = std::format("+ @{}", line.GetInstructionNumber());
+		if (i == m_Lines.size() - 1)
+		{
+			// Root of call stack
+			ss << rootPrefix << labelCountPrefix << "::" << line.GetText() << "" << "\n";
+			continue;
+		}
 
-    return ss.str();
+		strLine = std::format("{}::{}\n", labelCountPrefix, line.GetText());
+		strLine.insert(0, rootPrefixCount, ' ');
+		ss << strLine;
+	}
+	ss << "~~~~~~ End of Error ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+	return ss.str();
+}
+
+bool ErrorCallStack::GetErrorCallStackLineFromDbgFile(const ErrorCallStackLine& line, std::string* outTextLine) const
+{
+	if (DebugServer::Instance() == nullptr)
+	{
+		return false;
+	}
+
+	ScriptDebugInformation* debugInformation = DebugServer::Instance()->GetDebugInformationForScript(line.GetScriptSource());
+	if (debugInformation == nullptr)
+	{
+		return false;
+	}
+
+	FunctionDebugInformation* functionInformation = debugInformation->GetFunctionInformation(line.GetFunctionName());
+	if (functionInformation == nullptr)
+	{
+		return false;
+	}
+
+	size_t instructionOpcode = line.GetInstructionNumber();
+	size_t lineInfo = functionInformation->GetLineFromOpcode(instructionOpcode);
+	if (lineInfo == FunctionDebugInformation::NoLineInfo)
+	{
+		return false;
+	}
+
+	std::stringstream ss;
+	ss << std::format("At line {} in function '{}' of script '{}': \n", lineInfo, line.GetFunctionName(), debugInformation->GetOriginalFileName());
+
+	std::ifstream f(debugInformation->GetFullPath());
+	if (f.is_open())
+	{
+		std::string prev, next;
+		std::string lineText = get_line_at(f, lineInfo - 1, prev, next);
+		if (next != "") {
+			ss << "|\t" << prev << "\n";
+			ss << "|\t" << lineText << "\n";
+			ss << "L\t" << next << "\n";
+		}
+		else {
+			ss << "|\t" << prev << "\n";
+			ss << "L\t" << lineText << "\n";
+		}
+	}
+	else {
+		ss << "\t!Could not load load script text!\n";
+	}
+	f.close();
+
+	*outTextLine = ss.str();
+	return true;
 }
