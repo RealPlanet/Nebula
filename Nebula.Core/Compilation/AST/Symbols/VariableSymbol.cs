8847faed03af75dfac508d471f378033d460d182
Planet <fabioruscica@hotmail.it>
2026-03-30 00:02:48 +0200

    [Nebula.Core] Constant value of variable symbol can be changed after creation of symbol

--- Nebula.Core/Compilation/AST/Symbols/VariableSymbol.cs
+++ Nebula.Core/Compilation/AST/Symbols/VariableSymbol.cs
@@ -8,13 +8,18 @@ namespace Nebula.Core.Compilation.AST.Symbols
     {
         public bool IsReadOnly { get; }
         public TypeSymbol Type { get; }
-        public AbstractConstant? Constant { get; }
+        public AbstractConstant? Constant { get; private set; }
 
         public VariableSymbol(string @namespace, string name, bool isReadOnly, TypeSymbol variableType, AbstractConstant? constant)
             : base(@namespace, name)
         {
             IsReadOnly = isReadOnly;
             Type = variableType;
+            SetConstant(constant);
+        }
+
+        public void SetConstant(AbstractConstant? constant)
+        {
             Constant = IsReadOnly ? constant : null;
         }
     }
