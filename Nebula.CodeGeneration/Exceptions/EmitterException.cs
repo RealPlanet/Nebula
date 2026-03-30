a234e604503154137f80de6020b1614bb105fba7
Planet <fabioruscica@hotmail.it>
2026-03-30 21:26:17 +0200

    [Nebula.CodeGeneration] Added emitter exception

---
+++ Nebula.CodeGeneration/Exceptions/EmitterException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nebula.CodeGeneration.Exceptions
+{
+    public sealed class EmitterException
+        : Exception
+    {
+        public EmitterException(string? message)
+            : base(message)
+        {
+        }
+
+        public EmitterException()
+        {
+        }
+    }
+}
